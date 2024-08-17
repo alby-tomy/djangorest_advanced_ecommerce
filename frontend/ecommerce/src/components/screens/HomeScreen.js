5bf7d3e7227941850d0924e0919abf5d2a8d399d
alby-tomy <alby.u.tomy@gmail.com>
2024-08-17 23:00:02 +0530

    connecting backend frontend using axios

--- frontend/ecommerce/src/components/screens/HomeScreen.js
+++ frontend/ecommerce/src/components/screens/HomeScreen.js
@@ -1,11 +1,30 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
+import { Container } from "react-bootstrap";
+import axios from 'axios'
+import {Row, Col} from 'react-bootstrap'
 
 function HomeScreen(){
+    const [products, setProduct] = useState([])
+
+    useEffect(()=>{
+        async function fetchproducts() {
+            const {data} = await axios.get('/api/products')
+            setProduct(data)      
+        }
+        fetchproducts()
+    },[])
     return (
-        <div>
-            <h1>HomeScreen
-            </h1>
-        </div>
+        <Container>
+            <br />
+            <h1>Products</h1>
+            <Row>
+                {products.map((product)=>(
+                    <Col key={product._id} sm={12} md={6} lg={4} xl={3}>
+                        <h3>{product.product}</h3>
+                    </Col>
+                ))}
+            </Row>
+        </Container>
     )
 }
 export default HomeScreen
